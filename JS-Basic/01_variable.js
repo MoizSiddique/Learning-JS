0c1621503121bbc4d1f836c6b6dc5c35a78e666b
MoizSiddique <39435774+MoizSiddique@users.noreply.github.com>
2024-01-10 05:55:35 +0000

    Understand the variables

---
+++ JS-Basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 19070;
+let accountEmail = "example@gmail.com"
+var accountPassword = "98868"
+city = "lahore"
+
+// accountId = 896;
+console.log(accountId);
+city = 'karachi'
+console.log(city);
+
+console.table([accountId, accountEmail, accountPassword, city]);
